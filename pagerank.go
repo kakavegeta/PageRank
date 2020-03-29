80a5c9a3f9fa45dd331403029f23577ec52eeaa6
vegeka <zhj199305@gmail.com>
2020-03-29 04:45:22 -0400

    pagerank function finished

--- pagerank.go
+++ pagerank.go
@@ -1,15 +1,41 @@
 package pagerank
 
-import "gonum.org/v1/gonum/mat"
+import (
+	"math"
 
-func pageRank(P mat.Matrix, iterations int) *mat.Dense {
+	"gonum.org/v1/gonum/mat"
+)
+
+func pageRank(P mat.Matrix, tolerance float64) mat.Vector {
 	r, _ := P.Dims()
-	rank := Ones(r, 1)
-
-	for i := 1; i <= iterations; i++ {
-		rank.Mul(P.T(), rank)
+	data := make([]float64, r)
+	for i := range data {
+		data[i] = float64(1 / r)
 	}
+	rank := mat.NewVecDense(r, data)
+	rankPre := mat.NewVecDense(r, data)
+
+	for {
+		rank, rankPre = rankPre, rank
+		rank.MulVec(P, rankPre)
+		if vecDiff(rank, rankPre) < tolerance {
+			break
+		}
+	}
+
 	return rank
+}
+
+func vecDiff(a, b mat.Vector) float64 {
+	if a.Len() != b.Len() {
+		panic("Two vectors must have same length!")
+	}
+	var sum float64
+	for i := 0; i < a.Len(); i++ {
+		sum += math.Abs(b.AtVec(i) - a.AtVec(i))
+	}
+	return sum
+
 }
 
 func Ones(r, c int) *mat.Dense {
--- pagerank_test.go
+++ pagerank_test.go
@@ -1,12 +1,18 @@
 package pagerank
 
 import (
+	"fmt"
 	"testing"
 
 	"gonum.org/v1/gonum/mat"
 )
 
+func matPrint(X mat.Matrix) {
+	fa := mat.Formatted(X, mat.Prefix(""), mat.Squeeze())
+	fmt.Printf("%v\n", fa)
+}
+
 func TestOnes(t *testing.T) {
 	m := Ones(10, 3)
-	mat.Formatted(m)
+	matPrint(m)
 }
